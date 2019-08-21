c2da638636bca313a8389f78e225013e31d878af
Britton Smith <brittonsmith@gmail.com>
2019-08-21 16:02:28 +0100

    fix path.

--- treefarm/utilities/testing.py
+++ treefarm/utilities/testing.py
@@ -37,7 +37,7 @@ from treefarm.config import \
 if "YTREE_TEST_DATA_DIR" in os.environ:
     test_data_dir = os.environ["YTREE_TEST_DATA_DIR"]
 else:
-    test_data_dir = ytreecfg["treefarm"].get("test_data_dir", ".")
+    test_data_dir = treefarmcfg["treefarm"].get("test_data_dir", ".")
 generate_results = \
   int(os.environ.get("YTREE_GENERATE_TEST_RESULTS", 0)) == 1
 

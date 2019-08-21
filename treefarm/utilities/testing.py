aae42a239817b1df43782983d52fc183d2332ac1
Britton Smith <brittonsmith@gmail.com>
2019-08-21 22:37:12 +0100

    More removal.

--- treefarm/utilities/testing.py
+++ treefarm/utilities/testing.py
@@ -3,9 +3,7 @@ import os
 from treefarm.config import \
     treefarmcfg
 
-if "YTREE_TEST_DATA_DIR" in os.environ:
-    test_data_dir = os.environ["YTREE_TEST_DATA_DIR"]
+if "TREEFARM_TEST_DATA_DIR" in os.environ:
+    test_data_dir = os.environ["TREEFARM_TEST_DATA_DIR"]
 else:
     test_data_dir = treefarmcfg["treefarm"].get("test_data_dir", ".")
-generate_results = \
-  int(os.environ.get("YTREE_GENERATE_TEST_RESULTS", 0)) == 1

46ea876bffcc4e8fee28356828bd9abe21c88cab
Britton Smith <brittonsmith@gmail.com>
2019-08-21 15:58:51 +0100

    More renaming.

--- treefarm/utilities/testing.py
+++ treefarm/utilities/testing.py
@@ -31,13 +31,13 @@ from ytree.arbor.arbor import \
     load
 from ytree.arbor.frontends.ytree import \
     YTreeArbor
-from ytree.config import \
-    ytreecfg
+from treefarm.config import \
+    treefarmcfg
 
 if "YTREE_TEST_DATA_DIR" in os.environ:
     test_data_dir = os.environ["YTREE_TEST_DATA_DIR"]
 else:
-    test_data_dir = ytreecfg["ytree"].get("test_data_dir", ".")
+    test_data_dir = ytreecfg["treefarm"].get("test_data_dir", ".")
 generate_results = \
   int(os.environ.get("YTREE_GENERATE_TEST_RESULTS", 0)) == 1
 

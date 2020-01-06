7746366aa5e40ab0f1fac1d3aa316b3e6c0a0d0b
Britton Smith <brittonsmith@gmail.com>
2020-01-06 13:38:23 -0600

    Add codecov.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 dev_requirements = [
-    'coveralls', 'flake8', 'pytest>=3.6', 'pytest-cov', 'twine', 'wheel',
+    'codecov', 'flake8', 'pytest>=3.6', 'pytest-cov', 'twine', 'wheel',
     'sphinx', 'sphinx_rtd_theme']
 
 setup(name="treefarm",

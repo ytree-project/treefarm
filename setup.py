25f5de5de7854e642b26612c1eb5633385dfdcb6
Britton Smith <brittonsmith@gmail.com>
2020-01-06 13:27:51 -0600

    Update python version requirements.

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(name="treefarm",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
       ],
       install_requires=[
           'configparser',
@@ -66,5 +67,5 @@ setup(name="treefarm",
           'dev': dev_requirements,
           'rtd': [pkg for pkg in dev_requirements if 'sphinx' not in pkg],
       },
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*'
+      python_requires='>=3.5'
 )
